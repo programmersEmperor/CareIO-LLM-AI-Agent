20864ce603ade25d9a7421955614bbd25f8235b2
Mutasim <mutasim.business2020@gmail.com>
2023-08-29 20:23:53 +0300

    add custom prompt to the DBAIModel

--- AI/Models/DBAIModel.py
+++ AI/Models/DBAIModel.py
@@ -1,3 +1,4 @@
+from langchain import PromptTemplate
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.sql_database import SQLDatabase
@@ -21,6 +22,27 @@ class DBAIModel(IModel):
 )
 """,
     }
+    _template = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
+    Use the following format:
+
+    Question: "Question here"
+    SQLQuery: "SQL Query to run"
+    SQLResult: "Result of the SQLQuery"
+    Answer: "Final answer here"
+
+    Only use the following tables:
+
+    {table_info}
+
+    Rules:
+    You must never include any primary keys or foreign keys in the answer.
+    You must use SQL Like Operator when comparing with string value column.
+    You must return "Appointments" rows which their userId is equal to keyId.  
+    You must return "Users" rows which their userId is equal to keyId.  
+    Question: {input}"""
+    _prompt = PromptTemplate(
+        input_variables=["input", "table_info", "dialect"], template=_template
+    )
 
     def __init__(self, llm: ChatOpenAI):
         driver = 'ODBC+Driver+17+for+SQL+Server'
@@ -32,7 +54,7 @@ class DBAIModel(IModel):
         # conn = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}'
         conn = f'mssql+pyodbc:///?driver={driver}&server={server}&database={database}&trusted_connection=yes'
         db = SQLDatabase.from_uri(conn, include_tables=['Clients', 'Doctors', 'Appointments', 'Specialisms'], custom_table_info=self._custom_table_schema)
-        self._chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, return_direct=False, use_query_checker=True)
+        self._chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, return_direct=False, use_query_checker=True, prompt=self._prompt)
 
         # toolkit = SQLDatabaseToolkit(db=db, llm=llm)
         #
